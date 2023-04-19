6c20e6b3858b921ca291523ecc81a74d4fdc1f53
Lior Abir <liorabir909@gmail.com>
2023-04-19 16:07:06 +0300

    added basic scss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div className="App">
+			<div className="main-app">
+				<div className="top-bar"> top bar</div>
+				<nav className="main-nav"> main-nav</nav>
+				<div className="playing-bar">playing bar</div>
+				<div className="root-main"> main root</div>
+			</div>
+		</div>
+	)
+}
+
+export default App
---
+++ src/cmps/TopBar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class TopBar extends Component {
+	render() {
+		return (
+			<div className="top-bar">
+				<header className="top-bar-menu"></header>
+			</div>
+		)
+	}
+}
